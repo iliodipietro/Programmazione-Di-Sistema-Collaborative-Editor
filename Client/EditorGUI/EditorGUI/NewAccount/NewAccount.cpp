113961dbfad436eddab1811f13f8e0154c400266
Mattia Proietto <mattia_proietto@outlook.it>
2020-09-03 10:56:39 +0200

    changes in the UI

--- Client/EditorGUI/EditorGUI/NewAccount/NewAccount.cpp
+++ Client/EditorGUI/EditorGUI/NewAccount/NewAccount.cpp
@@ -2,6 +2,7 @@
 #include <QMouseEvent>
 #include <QMessageBox>
 
+#define RUBBER_SIZE 175
 NewAccount::NewAccount(QSharedPointer<SocketHandler> socketHandler, QWidget* parent)
 	: QMainWindow(parent), m_socketHandler(socketHandler),
 	m_timer(new QTimer(this))
@@ -32,7 +33,7 @@ void NewAccount::on_selectImageButton_clicked() {
 	if (url.compare("") != 0) {
 		m_selectedImage = new QPixmap(url);
 		m_resizedImage = new QPixmap(m_selectedImage->scaled(ui.imageLabel->size(), Qt::KeepAspectRatio));
-		QSize rubberSize(50, 50);
+		QSize rubberSize(RUBBER_SIZE , RUBBER_SIZE);
 		QPoint point(ui.imageLabel->pos());
 		QRect size(point, rubberSize);
 		if (m_selectionArea == Q_NULLPTR)
@@ -61,7 +62,7 @@ void NewAccount::on_submit_clicked() {
 
 		areaPos.setX(areaPos.x() - ui.imageLabel->pos().x());
 		areaPos.setY(areaPos.y() - ui.imageLabel->pos().y());
-		m_croppedImage = new QPixmap(m_resizedImage->copy(areaPos.x(), areaPos.y(), 50, 50));
+		m_croppedImage = new QPixmap(m_resizedImage->copy(areaPos.x(), areaPos.y(), RUBBER_SIZE, RUBBER_SIZE));
 		ui.crop->setPixmap(*m_croppedImage);
 		if (m_croppedImage != Q_NULLPTR) {
 			QJsonObject imageSerialized = Serialize::imageSerialize(*m_croppedImage, 2);
